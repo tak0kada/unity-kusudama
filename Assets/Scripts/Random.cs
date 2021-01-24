f8b083cf07bbe27612fb4cb43e916e1358ba5782
tak0kada <okokada.takuya+github@gmail.com>
2021-01-25 05:14:24 +0900

    fix avoid infinity

--- Assets/Scripts/Random.cs
+++ Assets/Scripts/Random.cs
@@ -11,6 +11,7 @@ public class Normal
     {
         // https://stackoverflow.com/questions/218060/random-gaussian-variables
         float u1 = 1.0f - (float)rnd.NextDouble();
+        u1 = u1 < 0.00001f ? 0.00001f : u1;
         float u2 = 1.0f - (float)rnd.NextDouble();
         return  mean + std * Mathf.Sqrt(-2.0f * Mathf.Log(u1)) * Mathf.Sin(2.0f * Mathf.PI * u2);
     }
