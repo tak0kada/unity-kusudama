914798a8145b7ce6d4009b6b5ee7e5e892d898a8
tak0kada <okokada.takuya+github@gmail.com>
2021-01-25 02:23:38 +0900

    add normal distribution

---
+++ Assets/Scripts/Random.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+
+public class Normal
+{
+    private static System.Random rnd = new System.Random();
+
+
+    public static float Next(float mean, float std)
+    {
+        // https://stackoverflow.com/questions/218060/random-gaussian-variables
+        float u1 = 1.0f - (float)rnd.NextDouble();
+        float u2 = 1.0f - (float)rnd.NextDouble();
+        return  mean + std * Mathf.Sqrt(-2.0f * Mathf.Log(u1)) * Mathf.Sin(2.0f * Mathf.PI * u2);
+    }
+}
